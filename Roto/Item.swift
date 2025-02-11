6dc6ff5d174e92c6322891437089345ca9812e78
mbridges <michael.bridges13@gmail.com>
2025-02-10 22:01:19 -0500

    Initial Commit

---
+++ Roto/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Roto
+//
+//  Created by Michael Bridges on 2/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ RotoTests/RotoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RotoTests.swift
+//  RotoTests
+//
+//  Created by Michael Bridges on 2/10/25.
+//
+
+import Testing
+@testable import Roto
+
+struct RotoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
